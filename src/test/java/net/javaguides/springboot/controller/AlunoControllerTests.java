2a29c4c838ac1536470ad22ce99b00fb3e28dd15
cmsjulio <cms.julio1@gmail.com>
2022-03-10 11:44:30 -0300

    Refatorando Controller teste: importando métodos static.

--- src/test/java/net/javaguides/springboot/controller/AlunoControllerTests.java
+++ src/test/java/net/javaguides/springboot/controller/AlunoControllerTests.java
@@ -3,23 +3,25 @@ package net.javaguides.springboot.controller;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import net.javaguides.springboot.model.Aluno;
 import net.javaguides.springboot.service.AlunoService;
-import org.hamcrest.CoreMatchers;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.mockito.ArgumentMatchers;
-import org.mockito.BDDMockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 import java.util.ArrayList;
 import java.util.List;
+
+
+import static org.mockito.BDDMockito.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.mockito.ArgumentMatchers.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.hamcrest.CoreMatchers.is;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.*;
 
 @WebMvcTest // carrega apenas os Beans necessários para testar o Controller.
 public class AlunoControllerTests {
@@ -38,78 +40,57 @@ public class AlunoControllerTests {
 
   @DisplayName("Controller Unit teste do endpoint criarAluno")
   @Test // pra que o JUnit detecte este método como um caso de teste JUnit.
-  public void dadoObjetoAluno_quandoCriarAluno_entaoRetornarAlunoSalvo() throws Exception{ //exceção do mockMvc.perform
+  public void dadoObjetoAluno_quandoCriarAluno_entaoRetornarAlunoSalvo() throws Exception { //exceção do mockMvc.perform
 
     // DADO - pré-condição ou setup
-    Aluno aluno = Aluno.builder()
-      .firstName("Julio")
-      .lastName("Silva")
-      .email("cms.julio1@gmail.com")
-      .build();
+    Aluno aluno = Aluno.builder().firstName("Julio").lastName("Silva").email("cms.julio1@gmail.com").build();
 
     // precisamos mockar o método do Service envolvido no controller
     // analisando o método criarAluno no controller, vemos que este chama o método alunoService.salvarAluno
     // este processo é chamado stubbing.
     // usamos o BDDMockito para tal.
 
-    BDDMockito.given(alunoService.salvarAluno(ArgumentMatchers.any(Aluno.class)))
-      .willAnswer((invocation) -> invocation.getArgument(0)); //retorna o argumento passado (posição 0, único arg.)
+    given(alunoService.salvarAluno(any(Aluno.class))).willAnswer((invocation) -> invocation.getArgument(0)); //retorna o argumento passado (posição 0, único arg.)
 
     // QUANDO - ação ou comportamento a ser testado
     // para fazermos a call do API REST, usamos o mockMvc
-    ResultActions response = mockMvc.perform(MockMvcRequestBuilders.post("http://localhost:8080/api/alunos")
-      .contentType(MediaType.APPLICATION_JSON)
-      .content(objectMapper.writeValueAsString(aluno)));
+    ResultActions response = mockMvc.perform(post("http://localhost:8080/api/alunos").contentType(MediaType.APPLICATION_JSON).content(objectMapper.writeValueAsString(aluno)));
 
     // ENTÃO - verficar o resultado ou saída utilizando assert.
-    response.andDo(MockMvcResultHandlers.print()) // para imprimir o output do test (request e o response)
-      .andExpect(MockMvcResultMatchers.status().isCreated()) //chacando se HTTP status é o CREATED
-      .andExpect(MockMvcResultMatchers.jsonPath("$.firstName",
-        CoreMatchers.is(aluno.getFirstName()))) //checando se saída do json bate com aluno.firstname
-      .andExpect(MockMvcResultMatchers.jsonPath("$.lastName",
-        CoreMatchers.is(aluno.getLastName())))
-      .andExpect(MockMvcResultMatchers.jsonPath("$.email",
-        CoreMatchers.is(aluno.getEmail())));
+    response.andDo(print()) // para imprimir o output do test (request e o response)
+      .andExpect(status().isCreated()) //chacando se HTTP status é o CREATED
+      .andExpect(jsonPath("$.firstName", is(aluno.getFirstName()))) //checando se saída do json bate com aluno.firstname
+      .andExpect(jsonPath("$.lastName", is(aluno.getLastName()))).andExpect(jsonPath("$.email", is(aluno.getEmail())));
 
   }
 
-    // Controller Unit teste do endpoint listarAlunos
-      @DisplayName("Controller Unit teste do endpoint listarAlunos")
-      @Test
-      public void dadoObjetosAluno_quandoListarAlunos_entaoRetornarListaDeAlunos() throws Exception{
+  // Controller Unit teste do endpoint listarAlunos
+  @DisplayName("Controller Unit teste do endpoint listarAlunos")
+  @Test
+  public void dadoObjetosAluno_quandoListarAlunos_entaoRetornarListaDeAlunos() throws Exception {
 
-        // DADO: pré-condição ou setup
-        // inserindo lista de alunos
-        Aluno aluno1 = Aluno.builder()
-          .firstName("Julio")
-          .lastName("Silva")
-          .email("cms.julio1@gmail.com")
-          .build();
+    // DADO: pré-condição ou setup
+    // inserindo lista de alunos
+    Aluno aluno1 = Aluno.builder().firstName("Julio").lastName("Silva").email("cms.julio1@gmail.com").build();
 
-        Aluno aluno2 = Aluno.builder()
-          .firstName("Juliana")
-          .lastName("Silva")
-          .email("js@gmail.com")
-          .build();
+    Aluno aluno2 = Aluno.builder().firstName("Juliana").lastName("Silva").email("js@gmail.com").build();
 
-        List<Aluno> listaDeAlunos = new ArrayList<>();
-        listaDeAlunos.add(aluno1);
-        listaDeAlunos.add(aluno2);
+    List<Aluno> listaDeAlunos = new ArrayList<>();
+    listaDeAlunos.add(aluno1);
+    listaDeAlunos.add(aluno2);
 
-        //stubbing
-        BDDMockito.given(alunoService.obterAlunos()).willReturn(listaDeAlunos);
+    //stubbing
+    given(alunoService.obterAlunos()).willReturn(listaDeAlunos);
 
 
-        // QUANDO: ação ou comportamento a ser testado
-        // usando o mockMvc para chamar o endpoint
-        ResultActions response = mockMvc.perform(MockMvcRequestBuilders.get("http://localhost:8080/api/alunos"));
+    // QUANDO: ação ou comportamento a ser testado
+    // usando o mockMvc para chamar o endpoint
+    ResultActions response = mockMvc.perform(get("http://localhost:8080/api/alunos"));
 
 
-        // ENTÃO: verificação das saídas
-        response.andDo(MockMvcResultHandlers.print())
-          .andExpect(MockMvcResultMatchers.status().isOk()) //pra verificar se o status do response é OK
-          .andExpect(MockMvcResultMatchers.jsonPath("$.size()",
-            CoreMatchers.is(listaDeAlunos.size()))); //pra verificar se o size do json de saída $.size é igual ao da saída esperada (listaDeAlunos)
+    // ENTÃO: verificação das saídas
+    response.andDo(print()).andExpect(status().isOk()) //pra verificar se o status do response é OK
+      .andExpect(jsonPath("$.size()", is(listaDeAlunos.size()))); //pra verificar se o size do json de saída $.size é igual ao da saída esperada (listaDeAlunos)
 
-      }
+  }
 }
